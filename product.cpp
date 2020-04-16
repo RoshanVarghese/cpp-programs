83f286257d9164bef1d95e510406d3d035a2c5db
RoshanVarghese <63452658+RoshanVarghese@users.noreply.github.com>
2020-04-16 17:04:25 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float a, b;
+    cout<<" PROGRAM TO FIND THE PRODUCT OF TWO NUMBERS ENTERED BY THE USER"<<endl;
+    cout<<" Enter two numbers:"<<endl;
+    cin >> a;
+    cin >> b;
+    cout<<"Product ="<<a*b<<endl;
+
+	return 0;
+}
