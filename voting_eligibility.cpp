00b6f77716da874a3c0f9200041377a127a34023
RoshanVarghese <63452658+RoshanVarghese@users.noreply.github.com>
2020-04-28 19:54:01 +0530

    Update voting_eligibility.cpp

--- voting_eligibility.cpp
+++ voting_eligibility.cpp
@@ -8,10 +8,10 @@ int main(){
     	cout<<"Are you here to check if you are eligible to vote ?"<<endl
     	    <<"1. Yes"<<endl
     	    <<"2. No"<<endl
-			<<"3. Exit"<<endl;
+	    <<"3. Exit"<<endl;
     	cout<<"Enter your choice"<<endl;
-		cin>>choice;
-		switch(choice){
+	cin>>choice;
+	switch(choice){
         	case 1: cout<<"Enter your age user:";
                 	cin>>age;
                 	if(age>=18){
@@ -21,11 +21,11 @@ int main(){
             		cout<<"You are not eligible to vote"<<endl;
                 	}
                 	break;
-            case 2: cout<<"Please give way for others and"<<endl;
+                case 2: cout<<"Please give way for others and"<<endl;
 			        break;
-			case 3: exit(0);        
-			default:cout<<"Invalid choice"<<endl;
-			        break;
+	        case 3: exit(0);        
+		default:cout<<"Invalid choice"<<endl;
+			break;
 	    }
         cout<<"Enter y if there are other users"<<endl;
         cin>>ch;
